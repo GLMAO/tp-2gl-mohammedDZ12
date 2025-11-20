130157f8166e51c92556ba4a1ba52b88f97eeb1b
mohammedSBA <madourifou22@gmail.com>
2025-11-20 14:23:02 +0100

    exercie 2

--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -3,19 +3,38 @@ package com.polytech.tp;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestionnaireEmploiDuTemps {
+public class GestionnaireEmploiDuTemps implements Subject {
     private List<ICours> listeCours = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
 
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
         // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
+        notifyObservers("Un nouveau cours a été ajouté : " + cours.getDescription());
     }
 
     public void modifierCours(ICours cours, String message) {
-        // Logique de modification...
         System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+        notifyObservers("Cours modifié : " + message);
+    }
+
+    @Override
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer o : observers) {
+            o.update(message);
+        }
     }
 
     public void setChangement(String string) {
